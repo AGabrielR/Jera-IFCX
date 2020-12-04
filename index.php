301ae045b53e3dfccf4db6df573c1e8aa526d0fe
AGabrielR <gabrielreis130@gmail.com>
2020-12-04 13:51:12 -0400

    Foram acrescentadas as bases dos arquivos js e index.php, além disso foi utilizado o bootstrap e um timer funcional de 25min.

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<link rel="stylesheet" href="css/bootstrap.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h1 class="m-4">
+        Timer pomodoro
+    </h1>
+
+    <div class="container">
+        <p class="pb-4 bg-dark col-md-6 offset-md-3 text-light text-center disabled display-1" id="timer">
+            Timer aqui
+        </p>
+    </div>
+</body>
+</html>
+<script src="js/bootstrap.js"></script>
+<script src="js/script.js"></script>
